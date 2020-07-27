8254c157e7dfb5b171de7be560071f0376fb139a
Julian Rivas <juliandarivas@hotmail.com>
2020-07-26 21:44:26 -0500

    Initial commit

---
+++ XMLExample/Directorio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace XMLExample
+{
+    public class Directorio
+    {
+        public Directorio(int id, string nombre, string apellido, string telefono)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Telefono = telefono;
+        }
+        public Directorio()
+        {
+
+        }
+
+
+        [XmlAttribute("Numero")]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        [XmlElement("Celular")]
+        public string Telefono { get; set; }
+    }
+}
---
+++ XMLExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace XMLExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenerarAPartirDeObjeto();
+            GenerarAPartirDeMultiplesObjetos();
+
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static void GenerarAPartirDeMultiplesObjetos()
+        {
+            List<Directorio> directorios = new List<Directorio>
+            {
+                new Directorio(1, "Andres", "Serna", "3015263365"),
+                new Directorio(1, "Felipe", "Rojas", "3015263365"),
+                new Directorio(1, "Andres", "Serna", "3015263365"),
+                new Directorio(1, "Felipe", "Rojas", "3015263365"),
+                new Directorio(1, "Felipe", "Serna", "3015263365"),
+                new Directorio(1, "Andres", "Rojas", "3015263365"),
+                new Directorio(1, "Felipe", "Serna", "3015263365"),
+                new Directorio(1, "Felipe", "Rojas", "3015263365"),
+                new Directorio(1, "Felipe", "Serna", "3015263365"),
+                new Directorio(1, "Andres", "Rojas", "3015263365"),
+                new Directorio(1, "Felipe", "Serna", "3015263365"),
+            };
+
+            XmlSerializer serializador = new XmlSerializer(directorios.GetType());
+
+            var ruta = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//directorios.xml";
+
+            FileStream archivo = File.Create(ruta);
+
+            serializador.Serialize(archivo, directorios);
+
+            archivo.Close();
+        }
+
+        private static void GenerarAPartirDeObjeto()
+        {
+            Directorio directorio = new Directorio(1, "Andres", "Serna", "3015263365");
+
+            XmlSerializer serializador = new XmlSerializer(typeof(Directorio));
+
+            var ruta = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//directorio.xml";
+
+            FileStream archivo = File.Create(ruta);
+
+            serializador.Serialize(archivo, directorio);
+
+            archivo.Close();
+        }
+    }
+}
